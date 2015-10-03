082740215cd72edf8f7f53e9fa10cf4c6dc3c2a4
sKabYY <skabyy@gmail.com>
2015-10-03 10:31:31 +0800

    修正建仓时资金是否充足判断错误的bug

--- quantdigger/kernel/engine/blotter.py
+++ quantdigger/kernel/engine/blotter.py
@@ -1,4 +1,4 @@
-# -*- coding: utf8 -*-
+# -*- coding: utf-8 -*-
 from abc import ABCMeta, abstractmethod
 
 from quantdigger.kernel.engine.event import OrderEvent, Event
@@ -196,7 +196,8 @@ class SimpleBlotter(Blotter):
             logger.warn("下单仓位问题")
             return False
         elif order.side == TradeSide.KAI:
-            if self.current_holdings['cash'] < order.price:
+            print '(%s %s)' % (self.current_holdings['cash'], order.price * order.quantity)
+            if self.current_holdings['cash'] < order.price * order.quantity:
                 raise Exception('没有足够的资金开仓') 
         return True
 
