1fb8528e8212a3e7ca06c6f3fe016cd07b04af12
sKabYY <skabyy@gmail.com>
2015-10-20 22:12:07 +0800

    新增海龟策略demo

--- quantdigger/demo/donchian_stg.py
+++ quantdigger/demo/donchian_stg.py
@@ -10,14 +10,13 @@ class DonChianStrategy(TradingStrategy):
     def __init__(self, exe):
         super(DonChianStrategy, self).__init__(exe)
         print 'start: ', self.datetime[0]
-
         self.b_upper, self.b_middler, self.b_lower = BOLL(self, self.close, 10,'boll10', 'y', '1')
         self.num_cont = 0
         self.num_win = 0
 
-    def __get_position_size(self):
+    def __get_position_size(self, price):
         P = 10  # 最少交易股数，这里假设是10
-        quantity = int(self.cash() / self.open / P) * P
+        quantity = int(self.cash() / price / P) * P
         return quantity
 
     def on_bar(self):
@@ -28,19 +27,20 @@ class DonChianStrategy(TradingStrategy):
         price = self.close[0]
         upper = max([self.close[i] for i in range(0, IN)])
         down = min([self.close[i] for i in range(0, OUT)])
-        if self.position() == 0 and price >= upper:
-            quantity = self.__get_position_size()
+        if self.position() == 0 and self.high >= upper:
+            quantity = self.__get_position_size(price)
             if quantity > 0:
-                self.buy('long', price, quantity, contract = code)
+                self.buy('long', price, quantity)
                 self.buy_price = price
                 self.num_cont += 1
                 #print 'buy', self.datetime[0].date(), price, quantity
-        elif self.position() > 0 and price <= down:
+        elif self.position() > 0 and self.low <= down:
             self.sell('long', price, self.position())
             #print 'sel', self.datetime[0].date(), price, self.position()
             #print '---'
             if price > self.buy_price:
                 self.num_win += 1
+
 
 if __name__ == '__main__':
     import sys
