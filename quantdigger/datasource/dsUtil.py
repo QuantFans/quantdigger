77e5684dc79ae806ebb570b60b551f0a1b3d5a19
sKabYY <skabyy@gmail.com>
2016-05-30 15:23:28 +0800

    [IoC] lazy construct

--- quantdigger/datasource/dsUtil.py
+++ quantdigger/datasource/dsUtil.py
@@ -4,15 +4,24 @@ from quantdigger.config import settings
 
 _ds_container = IoCContainer()
 
+
+class _DatasourceTrunk(IoCTrunk):
+
+    def __init__(self, cls, args, kwargs):
+        super(_DatasourceTrunk, self).__init__(cls, args, kwargs)
+
+    def on_register(self, name):
+        logger.info('register datasource: {cls} => {name}',
+                    cls=self.cls, name=name)
+
+    def construct(self):
+        a = [settings[k] for k in self.args]
+        ka = {k: settings[name] for k, name in self.kwargs.items()}
+        return self.cls(*a, **ka)
+
+
+register_datasource = register_to(_ds_container, _DatasourceTrunk)
 resolve_datasource = resolve_from(_ds_container)
-
-
-def register_datasource(name, *args, **kwargs):
-    logger.info('register datasource: {name}', name=name)
-    r = register_to(_ds_container)
-    a = [settings[k] for k in args]
-    ka = {k: settings[name] for k, name in kwargs.items()}
-    return r(name, *a, **ka)
 
 
 def get_setting_datasource():
--- quantdigger/infras/ioc.py
+++ quantdigger/infras/ioc.py
@@ -15,15 +15,36 @@ class IoCContainer(object):
             raise Exception(msg + str(self))
         return self._container[key]
 
+    def set(self, key, obj):
+        if key not in self._container:
+            msg = 'IoC Error: %s does not exist!\n\t' % key
+            raise Exception(msg + str(self))
+        self._container[key] = obj
+
     def __str__(self):
         return 'IoC: %s' % str(self._container)
 
 
-def register_to(ioc_container):
+class IoCTrunk(object):
+
+    def __init__(self, cls, args, kwargs):
+        self.cls = cls
+        self.args = args
+        self.kwargs = kwargs
+
+    def on_register(self, name):
+        pass
+
+    def construct(self):
+        raise NotImplementedError
+
+
+def register_to(ioc_container, trunk_cls):
     def register(name, *args, **kwargs):
         def decorator(cls):
-            instance = cls(*args, **kwargs)
-            ioc_container.register(name, instance)
+            trunk = trunk_cls(cls, args, kwargs)
+            trunk.on_register(name)
+            ioc_container.register(name, trunk)
             return cls
         return decorator
     return register
@@ -31,5 +52,11 @@ def register_to(ioc_container):
 
 def resolve_from(ioc_container):
     def resolve(name):
-        return ioc_container.resolve(name)
+        obj = ioc_container.resolve(name)
+        if isinstance(obj, IoCTrunk):
+            instance = obj.construct()
+            ioc_container.set(name, instance)
+            return instance
+        else:
+            return obj
     return resolve
