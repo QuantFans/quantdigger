bfcdd1a382f57ed9cefb8d7ea2d49f270ee2a461
wondereamer <wells7.wong@gmail.com>
2017-07-09 21:12:56 +0800

    优化on_symbol, on_bar测试代码

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -24,16 +24,14 @@ class TestSeries(unittest.TestCase):
     测试：
     * 序列变量
         1）用户时间，数字序列变量。
-        2）序列变量回溯。      ctx.open[3]
-        3) 序列变量的直接运算。ctx.open-0
-        4) NumberSeries.DEFAULT_VALUE
-        5) DateTimeSeries.DEFAULT_VALUE
-        6) open, close, high, low, volume, datetime等系统序列变量。
+        2）序列变量回溯引用。      ctx.open[3]
+        3) 序列变量和数值的运算。ctx.open-0
+        4) NumberSeries.DEFAULT_VALUE, DateTimeSeries.DEFAULT_VALUE
+        5) open, close, high, low, volume, datetime等系统序列变量值。
     * 普通变量。   ctx.curbar_list
     * ctx.curbar。  ctx.curbar_list
     """
     def test_case(self):
-        logger.info('***** 序列变量测试开始 *****')
         close, open, dt, high, low, volume = [], [], [], [], [], []
         open3, dt3 = [], []
         operator_test = []
@@ -53,7 +51,7 @@ class TestSeries(unittest.TestCase):
                 ctx.curbar_list = []
 
             def on_symbol(self, ctx):
-                # @TODO + - * /
+                # @TODO * /
                 operator_test.append(ctx.open - 0 == ctx.open[0])
                 operator_test.append(ctx.close - 0 == ctx.close[0])
                 operator_test.append(ctx.high + 0 == ctx.high[0])
@@ -144,19 +142,22 @@ class TestSeries(unittest.TestCase):
         logger.info('-- 序列变量测试成功 --')
 
 
-class TestIndicator(unittest.TestCase):
+class TestTechnical(unittest.TestCase):
 
     def test_case(self):
-        logger.info('***** 指标测试开始 *****')
-        close, open, ma2 = [], [], []
-        pre_ma2 = []
-        true_test = []
+        """
+        测试：
+        * 指标变量
+            1) 指标变量和数值间的运算。 ctx.ma2 - 0
+            2) 指标变量回溯  ctx.ma2[3]
+            3) 单值和多值测试
+        """
+        close, open, ma, ma3, tech_operator = [], [], [], [], []
         boll = {
             'upper': [],
             'middler': [],
             'lower': []
         }
-
         boll3 = {
             'upper': [],
             'middler': [],
@@ -166,16 +167,16 @@ class TestIndicator(unittest.TestCase):
         class DemoStrategy(Strategy):
             def on_init(self, ctx):
                 """初始化数据"""
-                ctx.ma2 = MA(ctx.close, 2)
+                ctx.ma = MA(ctx.close, 2)
                 ctx.boll = BOLL(ctx.close, 2)
 
             def on_symbol(self, ctx):
                 if ctx.curbar>=2:
-                    # @todo + - * /
-                    true_test.append(ctx.ma2 - 0 == ctx.ma2[0])
+                    # @todo + * /
+                    tech_operator.append(ctx.ma - 0 == ctx.ma[0])
 
-                pre_ma2.append(ctx.ma2[3])
-                ma2.append(ctx.ma2[0])
+                ma3.append(ctx.ma[3])
+                ma.append(ctx.ma[0])
                 close.append(ctx.close[0])
                 open.append(ctx.open[0])
                 boll['upper'].append(float(ctx.boll['upper']))
@@ -184,126 +185,61 @@ class TestIndicator(unittest.TestCase):
                 boll3['upper'].append(ctx.boll['upper'][3])
                 boll3['middler'].append(ctx.boll['middler'][3])
                 boll3['lower'].append(ctx.boll['lower'][3])
+                assert(isinstance(ctx.boll['lower'], NumberSeries))
+                assert(isinstance(ctx.ma, MA))
 
         set_symbols(['BB.TEST-1.Minute'])
         add_strategy([DemoStrategy('A1')])
         run()
-        self.assertTrue(all(true_test), "指标转化错误!")
-        logger.info('-- 指标转化测试成功 --')
 
-        # 分别测试单值和多值指标函数。
-        source_ma2 = talib.SMA(np.asarray(close), 2)
-        true_test, false_test = [], []
-        for i in xrange(0, len(close)):
-            if i >= 1:
-                true_test.append(ma2[i] == source_ma2[i])
-            else:
-                false_test.append(ma2[i] == ma2[i])
-
-        self.assertFalse(any(false_test), "单值指标正例测试失败!")
-        self.assertTrue(all(true_test), "单值指标正例测试失败!")
-        source_ma2 = talib.SMA(np.asarray(open), 2)
-        true_test, false_test = [], []
-        for i in xrange(0, len(open)):
-            if i >=  1:
-                true_test.append(ma2[i] == source_ma2[i])
-            else:
-                false_test.append(ma2[i] == ma2[i])
-        self.assertFalse(any(false_test), "单值指标反例测试失败!")
-        self.assertFalse(all(true_test), "单值指标反例测试失败!")
+        # 单值指标运算和回溯测试
+        source_ma = talib.SMA(np.asarray(close), 2)
+        self.assertTrue(all(tech_operator), "指标运算错误!")
+        self.assertFalse(ma[0] == ma[0], "指标NaN值测试失败!")
+        for source, target in zip(source_ma[1:], ma[1:]):
+            self.assertTrue(target == source, "单值指标计算测试失败!")
+        for source, target in zip(ma[1:], ma3[4:]):
+            self.assertTrue(target == source, "单值指标回溯测试失败!")
+        for nan in ma3[:4]:
+            self.assertFalse(nan == nan, "单值指标回溯NaN值测试失败!")
         logger.info('-- 单值指标测试成功 --')
 
-        true_test, false_test = [], []
-        source_ma2 = talib.SMA(np.asarray(close), 2)
-        for i in xrange(0, len(close)):
-            if i >= 4:
-                true_test.append(pre_ma2[i] == source_ma2[i-3])
-            else:
-                # 指标序列变量的默认值为nan
-                false_test.append(pre_ma2[i] == pre_ma2[i])
-        self.assertTrue(all(true_test), "单值指标回溯正例测试失败")
-        self.assertFalse(any(false_test), "单值指标回溯正例测试失败")
-        true_test, false_test = [], []
-        source_ma2 = talib.SMA(np.asarray(open), 2)
-        for i in xrange(0, len(close)):
-            if i >= 4:
-                true_test.append(pre_ma2[i] == source_ma2[i-3])
-            else:
-                # 指标序列变量的默认值为nan
-                false_test.append(pre_ma2[i] == pre_ma2[i])
-        self.assertFalse(all(true_test), "单值指标回溯反例测试失败")
-        self.assertFalse(any(false_test), "单值指标回溯反例测试失败")
-        logger.info('-- 单值指标回溯测试成功 --')
-
-        #
-        u, m, l = talib.BBANDS(np.asarray(close), 2, 2, 2)
-        true_test, false_test = [], []
-        for i in xrange(0, len(close)):
-            if i >=  1:
-                true_test.append(boll['upper'][i] == u[i])
-                true_test.append(boll['middler'][i] == m[i])
-                true_test.append(boll['lower'][i] == l[i])
-            else:
-                false_test.append(boll['upper'][i] == boll['upper'][i])
-                false_test.append(boll['middler'][i] == boll['middler'][i])
-                false_test.append(boll['lower'][i] == boll['lower'][i])
-        self.assertFalse(any(false_test), "多值指标正例测试失败!")
-        self.assertTrue(all(true_test), "多值指标正例测试失败!")
-        u, m, l = talib.BBANDS(np.asarray(open), 2, 2, 2)
-        true_test, false_test = [], []
-        for i in xrange(0, len(close)):
-            if i >=  1:
-                true_test.append(boll['upper'][i] == u[i])
-                true_test.append(boll['middler'][i] == m[i])
-                true_test.append(boll['lower'][i] == l[i])
-            else:
-                false_test.append(boll['upper'][i] == boll['upper'][i])
-                false_test.append(boll['middler'][i] == boll['middler'][i])
-                false_test.append(boll['lower'][i] == boll['lower'][i])
-        self.assertFalse(any(false_test), "多值指标反例测试失败!")
-        self.assertFalse(all(true_test), "多值指标反例测试失败!")
+        # 多值指标运算和回溯测试
+        upper, middler, lower = talib.BBANDS(np.asarray(close), 2, 2, 2)
+        ta_boll = {
+            'upper': upper,
+            'middler': middler,
+            'lower': lower
+        }
+        for v in ['upper', 'lower', 'middler']:
+            self.assertFalse(boll[v][0] == boll[v][0], "多值指标NaN值测试失败!")
+            for source, target in zip(ta_boll[v][1:], boll[v][1:]):
+                self.assertTrue(target == source, "多值指标计算测试失败!")
+            for nan in boll3[v][:4]:
+                self.assertFalse(nan == nan, "多值指标回溯NaN值测试失败!")
+            for source, target in zip(boll[v][1:], boll3[v][4:]):
+                self.assertTrue(target == source, "多值指标回溯测试失败!")
         logger.info('-- 多值指标测试成功 --')
+        logger.info('***** 指标测试成功 *****\n')
 
 
-        true_test, false_test = [], []
-        u, m, l = talib.BBANDS(np.asarray(close), 2, 2, 2)
-        for i in xrange(0, len(close)):
-            if i >= 4:
-                true_test.append(boll3['upper'][i] ==  u[i-3])
-            else:
-                false_test.append(boll3['upper'][i] ==  boll3['upper'][i])
-        self.assertTrue(all(true_test), "多值指标回溯正例测试失败")
-        self.assertFalse(any(false_test), "多值指标回溯正例测试失败")
-        true_test, false_test = [], []
-        u, m, l = talib.BBANDS(np.asarray(open), 2, 2, 2)
-        for i in xrange(0, len(close)):
-            if i >= 4:
-                true_test.append(boll3['upper'][i] == u[i-3])
-            else:
-                false_test.append(boll3['upper'][i] == boll3['upper'][i])
-        self.assertFalse(all(true_test), "多值指标回溯反例测试失败")
-        self.assertFalse(any(false_test), "多值指标回溯反例测试失败")
-        logger.info('-- 多值指标回溯测试成功 --')
-        logger.info('***** 指标测试结束 *****\n')
-
-
-class TestMultipleCombination(unittest.TestCase):
-    """ 多组合策略, 测试数据、策略遍历
-    """
-
+class TestMainFunction(unittest.TestCase):
     def test_case(self):
-        logger.info('***** 多组合策略测试开始 *****')
+        """
+        案例：两个策略组合，每个策略组合下分别有两个策略，每个组合运行于两个周期合约中。
+        测试：on_bar, on_symbol, on_exit 的运行频次，数据和策略遍历的粗粒度测试;
+              ctx.prontract, ctx.strategy
+        """
         on_exit = {
-                'strategy': [],
-                }
-
+            'strategy': [],
+        }
         on_bar = {
-                'strategy': [],
-                }
+            'strategy': [],
+        }
         on_symbol = {
-                'combination': set(),
-                'count': 0
-                }
+            'combination': set(),
+            'step_num': 0
+        }
 
         class DemoStrategy(Strategy):
 
@@ -312,17 +248,15 @@ class TestMultipleCombination(unittest.TestCase):
                 return
 
             def on_symbol(self, ctx):
-                #print ctx.strategy, ctx.pcontract
+                # print ctx.strategy, ctx.pcontract
                 on_symbol['combination'].add((str(ctx.pcontract), ctx.strategy))
-                on_symbol['count'] += 1
-                pass
+                on_symbol['step_num'] += 1
 
             def on_bar(self, ctx):
                 on_bar['strategy'].append(ctx.strategy)
 
             def on_exit(self, ctx):
                 on_exit['strategy'].append(ctx.strategy)
-                return
 
         set_symbols(['BB.TEST-1.Minute', 'AA.TEST-1.Minute'])
         add_strategy([DemoStrategy('A1'), DemoStrategy('A2')])
@@ -334,48 +268,37 @@ class TestMultipleCombination(unittest.TestCase):
         fname = os.path.join(os.getcwd(), 'data', '1MINUTE', 'TEST', 'AA.csv')
         alen = len(pd.read_csv(fname))
         sample = set([
-                ('BB.TEST-1.MINUTE', 'A1'),
-                ('BB.TEST-1.MINUTE', 'A2'),
-                ('AA.TEST-1.MINUTE', 'A1'),
-                ('AA.TEST-1.MINUTE', 'A2'),
-                ('BB.TEST-1.MINUTE', 'B1'),
-                ('BB.TEST-1.MINUTE', 'B2'),
-                ('AA.TEST-1.MINUTE', 'B1'),
-                ('AA.TEST-1.MINUTE', 'B2')
+            ('BB.TEST-1.MINUTE', 'A1'),
+            ('BB.TEST-1.MINUTE', 'A2'),
+            ('AA.TEST-1.MINUTE', 'A1'),
+            ('AA.TEST-1.MINUTE', 'A2'),
+            ('BB.TEST-1.MINUTE', 'B1'),
+            ('BB.TEST-1.MINUTE', 'B2'),
+            ('AA.TEST-1.MINUTE', 'B1'),
+            ('AA.TEST-1.MINUTE', 'B2')
         ])
-        self.assertTrue(on_symbol['combination'] == sample)
-        sample.pop()
-        self.assertFalse(on_symbol['combination'] == sample)
-        self.assertTrue(on_symbol['count'] == alen*4 + blen*4)
-        self.assertFalse(on_symbol['count'] == alen*3 + blen*4)
-        self.assertTrue(['A1', 'A2', 'B1', 'B2']*max(blen, alen) == on_bar['strategy'],
-                        'on_bar测试失败！')
-        self.assertFalse(['C1', 'A2', 'B1', 'B2']*max(blen, alen) == on_bar['strategy'],
+        self.assertTrue(alen > 0 and blen > 0)
+
+        # 测试on_symbol
+        self.assertTrue(on_symbol['combination'] == sample, "on_symbol测试失败!")
+        self.assertTrue(on_symbol['step_num'] == alen * 4 + blen * 4, "on_symbol测试失败!")
+        self.assertTrue(['A1', 'A2', 'B1', 'B2'] * max(blen, alen) == on_bar['strategy'],
                         'on_bar测试失败！')
         self.assertTrue(['A1', 'A2', 'B1', 'B2'] == on_exit['strategy'], 'on_exit测试失败！')
-        self.assertFalse(['C1', 'A2', 'B1', 'B2'] == on_exit['strategy'], 'on_exit测试失败！')
-        logger.info('-- 多组合策略测试成功 --')
+        logger.info('-- 策略on_xxx主函数测试成功 --')
 
 
-class TestDiffPeriod(unittest.TestCase):
-    """ 跨周期多组合策略测试
-        主要测试时间对齐和数据、策略遍历
+class TestTimeAlign(unittest.TestCase):
+    """
+        案例：不同时间长度和不同时间步频的数据下策略组合的合约时间对齐和数据引用。
+        测试：合约名称大小写不敏感。
+              on_symbol时间对齐
+              on_bar时间对齐, 跨合约数据引用（只测了时间）
     """
 
     def test_case(self):
-        logger.info('***** 跨周期多组合策略测试开始 *****')
-        on_exit = {
-                'strategy': [],
-                }
-
-        on_bar = {
-                'strategy': [],
-                'diffPeriod': [],
-                }
-        on_symbol = {
-                'diffPeriod': [],
-                'count': 0
-                }
+        on_bar_timestep = []
+        on_symbol_timestep = []
 
         class DemoStrategy(Strategy):
 
@@ -384,92 +307,42 @@ class TestDiffPeriod(unittest.TestCase):
                 return
 
             def on_symbol(self, ctx):
-                on_symbol['count'] += 1
-                on_symbol['diffPeriod'].append("%s %s %s %s"%(ctx.pcontract,
-                    ctx.strategy, ctx.datetime, ctx.curbar))
-                pass
+                on_symbol_timestep.append("%s %s %s %s" % (ctx.pcontract,
+                                          ctx.strategy, ctx.datetime, ctx.curbar))
 
             def on_bar(self, ctx):
-                on_bar['strategy'].append(ctx.strategy)
                 t = ctx['oneday.TEST-1.Minute']
-                on_bar['diffPeriod'].append("%s %s %s"%(t.pcontract, t.datetime, t.curbar))
+                on_bar_timestep.append("%s %s %s" % (t.pcontract, t.datetime, t.curbar))
                 t = ctx['TWODAY.TEST-5.Second']
-                on_bar['diffPeriod'].append("%s %s %s"%(t.pcontract, t.datetime, t.curbar))
-
-            def on_exit(self, ctx):
-                on_exit['strategy'].append(ctx.strategy)
-                return
+                on_bar_timestep.append("%s %s %s" % (t.pcontract, t.datetime, t.curbar))
 
         set_symbols(['TWODAY.TEST-5.Second', 'oneday.TEST-1.Minute'])
-        # 每个策略的没个时间点会允许一次on_bar。
         add_strategy([DemoStrategy('A1'), DemoStrategy('A2')])
         add_strategy([DemoStrategy('B1'), DemoStrategy('B2')])
         run()
 
         # on_symbol
-        fname = os.path.join(os.getcwd(), 'data', '1MINUTE', 'TEST', 'ONEDAY.csv')
-        blen = len(pd.read_csv(fname))
-        fname = os.path.join(os.getcwd(), 'data', '5SECOND', 'TEST', 'TWODAY.csv')
-        alen = len(pd.read_csv(fname))
-        self.assertTrue(on_symbol['count'] == alen*4 + blen*4)
-        self.assertFalse(on_symbol['count'] == alen*3 + blen*4)
         fname = os.path.join(os.getcwd(), 'data', 'diffPeriodOnSymbol.txt')
-        with open(fname) as f:
-            source =  f.readlines()
-            assert(len(source)>0 and source[0] != "")
-            i = 0
-            for t in on_symbol['diffPeriod']:
-                if source[i].startswith('*'):
-                    i+=1
-                self.assertTrue(t == source[i].rstrip('\n'), "跨周期合约on_symbol失败")
-                i+=1
+        lines = [line.rstrip('\n') for line in open(fname)]
+        assert(len(lines) > 0)
+        count = 0
+        for line in lines:
+            if line.startswith("*"):
+                continue
+            self.assertTrue(on_symbol_timestep[count] == line, "on_symbol时间对齐失败")
+            count += 1
 
         # on_bar
         fname = os.path.join(os.getcwd(), 'data', 'diffPeriodOnBar.txt')
-        with open(fname) as f:
-            source =  f.readlines()
-            assert(len(source)>0 and source[0] != "")
-            i = 0
-            for t in on_bar['diffPeriod']:
-                if source[i].startswith('*'):
-                    i+=1
-                self.assertTrue(t == source[i].rstrip('\n'), "跨周期合约引用失败")
-                i+=1
-
-        self.assertTrue(['A1', 'A2', 'B1', 'B2'] * 6498 == on_bar['strategy'],
-                        'on_bar测试失败！')
-        self.assertFalse(['C1', 'A2', 'B1', 'B2'] * 6498 == on_bar['strategy'],
-                        'on_bar测试失败！')
-
-        # on_exit
-        self.assertTrue(['A1', 'A2', 'B1', 'B2'] == on_exit['strategy'], 'on_exit测试失败！')
-        self.assertFalse(['C1', 'A2', 'B1', 'B2'] == on_exit['strategy'], 'on_exit测试失败！')
-        logger.info('-- 跨周期多组合策略测试成功 --')
-
-        logger.info('***** 跨周期多组合策略测试结束 *****\n')
-
-
-class TestPContractsWithSameContract(unittest.TestCase):
-
-    def test_case(self):
-        logger.info('***** 序列变量测试开始 *****')
-
-        class DemoStrategy(Strategy):
-
-            def on_init(self, ctx):
-                """初始化数据"""
-                return
-
-            def on_bar(self, ctx):
-                # 相同合约不同周期的数据，当前价总是等于最小周期的数据。
-                assert(ctx.open == ctx['TWODAY.TEST-5.Second'].open)
-                assert(ctx.close == ctx['TWODAY.TEST-5.Second'].close)
-                assert(ctx.high == ctx['TWODAY.TEST-5.Second'].high)
-                assert(ctx.low == ctx['TWODAY.TEST-5.Second'].low)
-
-        set_symbols(['TWODAY.TEST-5.Second', 'TWODAY.TEST-1.Minute'])
-        add_strategy([DemoStrategy('A1')])
-        run()
+        lines = [line.rstrip('\n') for line in open(fname)]
+        assert(len(lines) > 0)
+        count = 0
+        for line in lines:
+            if line.startswith("*"):
+                continue
+            self.assertTrue(on_bar_timestep[count] == line, "on_bar时间对齐失败")
+            count += 1
+        logger.info('on_symbol, on_bar 时间对齐测试成功！')
 
 
 if __name__ == '__main__':
