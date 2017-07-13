f670a5b674992c7cad88b14ee2a9fd6abc1c5508
DESKTOP-LNPG7HN\Acer <qink1986@gmail.com>
2017-07-14 00:37:01 +0800

    fix bug

--- demo/main.py
+++ demo/main.py
@@ -21,7 +21,7 @@ time.sleep(1)
 log.info("启动ipython..")
 shell = subprocess.call('ipython --config=%s' % shell_path, shell=True)
 ###notebook = subprocess.call('jupyter notebook --config=shell.py', shell=True)
-###prin(t mainwindow.pid)
+###print(mainwindow.pid)
 
 input("Any key to quit quantdigger.")
 subprocess.Popen.kill(mainwindow)
